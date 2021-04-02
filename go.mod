1e684c1f33be2b34bff1f8923aef165077f5563a
bruno-chavez <bruno.chavezl@sansano.usm.cl>
2021-04-02 01:44:55 -0300

    Made cors handling optional.

--- go.mod
+++ go.mod
@@ -5,5 +5,4 @@ go 1.15
 require (
 	github.com/joho/godotenv v1.3.0
 	github.com/julienschmidt/httprouter v1.3.0
-	github.com/rs/cors v1.7.0
 )
--- server/server.go
+++ server/server.go
@@ -2,13 +2,13 @@ package server
 
 import (
 	"github.com/julienschmidt/httprouter"
-	"github.com/rs/cors"
 	"go-microservice-template/handlers"
 	"net/http"
 	"os"
 	"time"
 )
 
+// NewServer creates and return a http.Server struct, sets timeouts, register routes and optionally adds CORS handling.
 func NewServer(h *handlers.Handler) *http.Server {
 
 	router := httprouter.New()
@@ -16,17 +16,18 @@ func NewServer(h *handlers.Handler) *http.Server {
 	// health check
 	router.GET("/info", h.Info())
 
-	// enables CORS
-	corsConfig := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000"},
-		AllowedMethods:   []string{"POST", "GET", "DELETE", "PUT", "PATCH"},
-		AllowCredentials: true,
-	})
-	handlerWithCORS := corsConfig.Handler(router)
+	// Enables CORS. If the functionality is needed just add this section back.
+	// be sure download the module before running your server!
+	/*	corsConfig := cors.New(cors.Options{
+			AllowedOrigins:   []string{"http://localhost:3000"},
+			AllowedMethods:   []string{"POST", "GET", "DELETE", "PUT", "PATCH"},
+			AllowCredentials: true,
+		})
+		handlerWithCORS := corsConfig.Handler(router)*/
 
 	srvr := &http.Server{
 		Addr:         ":" + os.Getenv("PORT"),
-		Handler:      handlerWithCORS,
+		Handler:      router, // replace it with handlerWithCORS if cors is needed
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 5 * time.Second,
 		IdleTimeout:  120 * time.Second,
